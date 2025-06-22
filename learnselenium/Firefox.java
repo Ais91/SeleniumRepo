7ac38644f8fae6cd921e095d8e9f7dab4477d1fd
Ais91 <167617041+Ais91@users.noreply.github.com>
2025-06-22 11:36:44 +0530

    Add files via upload
    Week1Day1_files

---
+++ learnselenium/Firefox.java
@@ -0,0 +1,21 @@
+package learnselenium;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double browserVersion = 100.2;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		System.out.println(browserVersion );
+		System.out.println(browserName );
+		System.out.println(isVisible );
+		System.out.println(releaseYear );
+		System.out.println(browserLogo );
+
+	}
+
+}
---
+++ learnselenium/week1day1task.java
@@ -0,0 +1,10 @@
+package learnselenium;
+
+public class week1day1task {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Testleaf");
+	}
+
+}
